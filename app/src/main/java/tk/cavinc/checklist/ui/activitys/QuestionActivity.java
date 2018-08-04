1da62816358b29f42410a49b2f4855fd68b192b8
CavInc <cavinc20@gmail.com>
2018-08-04 20:25:52 +0700

    Работа над дизайном

---
+++ app/src/main/java/tk/cavinc/checklist/ui/activitys/QuestionActivity.java
@@ -0,0 +1,15 @@
+package tk.cavinc.checklist.ui.activitys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import tk.cavinc.checklist.R;
+
+public class QuestionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question);
+    }
+}
